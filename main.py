4d898e85cdcee7c68c1e61d7dfe7b5090c4d3872
Dennis <dnns.mldr@gmail.com>
2023-01-01 18:24:42 +0100

    first version

--- main.py
+++ main.py
@@ -1,15 +1,74 @@
 """
 Analysing Klaverjassen cardgame to improve CPU-player
 
-Instruction: roem[0] = non-roem; roem[1] = roem;
+Instruction: troef[0/nee] = non-troef; troef[1/ja] = troef;
 Cards are: ['Ace', 'King', 'Queen', 'Jack', '10', '9', '8', '7'] # as strings!
+i.e.: points_per_card('Jack', troef['nee'])
+
+a 'game' is the total rounds played whom ever first reach 2592 points (=4 players x 4 hands x 162 points per hand).
+a 'hand' is playing 8 rounds/ cards after dealing (resulting in all cards played!)
+a 'round' is when each of the 4 players each play 1 card
+
+the main player (starts with north, each hand passes clockwise)
 """
 from functions import *
-from rules import suits
+import pandas as pd
 
-roem_suit = suits[str(input('Whats the roem-suit? ')).lower()]
-print(roem_suit)
+# Start a new game
+# ## Basic parameters
+Hand_number = 1
 
-print(points_per_card('Jack', roem[0]) + points_per_card('9', roem[1]))
+# ## register players/ teams north-south and east-west.
+player_north = str(input("Who is the first main player (player 'north')? "))
+player_east = str(input("Who is the following player (player 'east')? "))
+player_south = str(input("Who is the following player (player 'south')? "))
+player_west = str(input("Who is the last player (player 'west')? "))
+team_North_South = player_north + ' ' + player_south
+team_East_West = player_east + ' ' + player_west
+# ## creating game_scoring_table
+game_scoring_table = pd.DataFrame(columns=[team_North_South, team_East_West])
 
 
+# ## testing
+# TODO delete testing when done
+game_scoring_table.loc[len(game_scoring_table.index)] = [2, 3]
+game_scoring_table.loc[len(game_scoring_table.index)] = [2, 3]
+game_scoring_table.loc[len(game_scoring_table.index)] = [2, 3]
+print(f"{player_north}, {player_east}, {player_south}, {player_west}.", end="\n\n")
+print(game_scoring_table)
+
+# start a new hand, deck is shuffled, each player receives 8 cards.
+
+# ## Basic parameters
+cards_played_record = pd.DataFrame(columns=['hand_number',
+                                            'round_number',
+                                            'round_player',
+                                            'card_north',
+                                            'card_east',
+                                            'card_south',
+                                            'card_west',
+                                            'points-north_south',
+                                            'points_east_west'])
+round_number = 1
+
+while Hand_number < 17:
+    main_player = main_players(player_north, player_east, player_south, player_west, Hand_number)
+
+    print(f"The hand number is: {Hand_number}")
+    if Hand_number > 1:
+        print(f"    Score team {player_north}/ {player_south}: {game_scoring_table.iloc[:, 0].sum()}.")
+        print(f"    Score team {player_east}/ {player_west}: {game_scoring_table.iloc[:, 1].sum()}.")
+    print(f'The main player is (deals first card):{main_player}')
+
+    # ask for troef_suit
+    troef_suit = suits()
+    print(troef_suit, end='\n\n')
+
+
+
+    Hand_number += 1
+    # TODO determine main_player
+    # TODO input each card
+    # TODO record played cards in a pd.DataFrame
+    #  ## determine per hand winner en end-points
+    #  ## add final score to game_scoring_table
--- rules.py
+++ rules.py
@@ -1,27 +1,38 @@
-suits = {'clubs': 'Clubs',
-         'spades': 'Spades',
-         'hearts': 'Hearts',
-         'diamonds': 'Diamonds'}
+div_suits = {'clubs': 'Clubs',
+             'spades': 'Spades',
+             'hearts': 'Hearts',
+             'diamonds': 'Diamonds',
+             'c': 'Clubs',
+             's': 'Spades',
+             'h': 'Hearts',
+             'd': 'Diamonds'
+             }
 
-roem = {0: 'non_roem', 1: 'roem'}
+troef = {0: 'non_troef',
+         1: 'troef',
+         'nee': 'non_troef',
+         'ja': 'troef',
+         'Nee': 'non_troef',
+         'Ja': 'troef'
+         }
 
-rules_roem = {'Ace': {'hierarchy_roem': 3, 'points_value_roem': 11,
-                      'hierarchy_non_roem': 1, 'points_value_non_roem': 11},
-              '10': {'hierarchy_roem': 4, 'points_value_roem': 10,
-                     'hierarchy_non_roem': 2, 'points_value_non_roem': 10},
-              'King': {'hierarchy_roem': 5, 'points_value_roem': 4,
-                       'hierarchy_non_roem': 3, 'points_value_non_roem': 4},
-              'Queen': {'hierarchy_roem': 6, 'points_value_roem': 3,
-                        'hierarchy_non_roem': 4, 'points_value_non_roem': 3},
-              'Jack': {'hierarchy_roem': 1, 'points_value_roem': 20,
-                       'hierarchy_non_roem': 5, 'points_value_non_roem': 2},
-              '9': {'hierarchy_roem': 2, 'points_value_roem': 14,
-                    'hierarchy_non_roem': 6, 'points_value_non_roem': 0},
-              '8': {'hierarchy_roem': 7, 'points_value_roem': 0,
-                    'hierarchy_non_roem': 7, 'points_value_non_roem': 0},
-              '7': {'hierarchy_roem': 8, 'points_value_roem': 0,
-                    'hierarchy_non_roem': 8, 'points_value_non_roem': 0}
-              }
+hierarchy_value_rules = {'Ace': {'hierarchy_troef': 3, 'points_value_troef': 11,
+                                 'hierarchy_non_troef': 1, 'points_value_non_troef': 11},
+                         '10': {'hierarchy_troef': 4, 'points_value_troef': 10,
+                                'hierarchy_non_troef': 2, 'points_value_non_troef': 10},
+                         'King': {'hierarchy_troef': 5, 'points_value_troef': 4,
+                                  'hierarchy_non_troef': 3, 'points_value_non_troef': 4},
+                         'Queen': {'hierarchy_troef': 6, 'points_value_troef': 3,
+                                   'hierarchy_non_troef': 4, 'points_value_non_troef': 3},
+                         'Jack': {'hierarchy_troef': 1, 'points_value_troef': 20,
+                                  'hierarchy_non_troef': 5, 'points_value_non_troef': 2},
+                         '9': {'hierarchy_troef': 2, 'points_value_troef': 14,
+                               'hierarchy_non_troef': 6, 'points_value_non_troef': 0},
+                         '8': {'hierarchy_troef': 7, 'points_value_troef': 0,
+                               'hierarchy_non_troef': 7, 'points_value_non_troef': 0},
+                         '7': {'hierarchy_troef': 8, 'points_value_troef': 0,
+                               'hierarchy_non_troef': 8, 'points_value_non_troef': 0}
+                         }
 
 
 
