db6de6d232ca10f04abaa0b65acee85500413d88
Dennis M <61027427+dennismulder1978@users.noreply.github.com>
2023-01-01 12:48:01 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+Analysing Klaverjassen cardgame to improve CPU-player
+'''
+print('AI klaverjas cardgame analysis')
+
+# rules key = cardname; value is list: [0] Hierarchy (int), [1] point values (int).
+rules_roem = { 'Jack': [1,20],
+               'Nel': [2,14],
+               'Ace': [3,11],
+               '10': [4,10],
+               'King': [5,4],
+               'Queen': [6,3],
+               '8': [7,0],
+               '7': [8,0] }
+
+rules_non_roem = { 'Ace': [1,11],
+                   '10': [2,10],
+                   'King': [3,4],
+                   'Queen': [4,3],
+                   'Jack': [5,2],
+                   '9': [6,0],                  
+                   '8': [7,0],
+                   '7': [8,0] }
